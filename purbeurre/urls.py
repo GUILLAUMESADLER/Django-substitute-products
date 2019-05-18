9ee07e7d272e10f6741fab20b31b23800abc0a35
theodorax <guillaume.sadler@gmail.com>
2019-05-18 16:43:25 +0200

    url rearengement

--- purbeurre/urls.py
+++ purbeurre/urls.py
@@ -1,7 +1,7 @@
-"""purbeurre URL Configuration
+"""purbeurreV2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,29 +13,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.conf.urls import url, include
+from django.urls import path, include
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.conf import settings
-from django.conf.urls.static import static
+
+from substitutes import views as sub_views
 from users import views as user_views
-from search import views as search_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', search_views.index, name='index'),
-    path('search/results/', search_views.results, name='results'),
-    path('search/product/<int:id>', search_views.product, name='product'),
-    path('search/substitutes/<int:id>', search_views.substitutes, name='substitutes'),
-    path('search/favorites/<int:id>', search_views.add_favorite, name='add_favorite'),
-    path('search/favorites', search_views.favorites, name='favorites'),
-    path('del_favorite/<int:id>', search_views.del_favorite, name='del_favorite'),
+    # SUBSTITUTES APP
+    path('', sub_views.index, name='index'),
+    path('results/', sub_views.results, name='results'),
+    path('product/<int:id>', sub_views.product, name='product'),
+    path('substitutes/<int:id>', sub_views.substitutes, name='substitutes'),
+    path('favorites', sub_views.favorites, name='favorites'),
+    path('del_favorite/<int:id>', sub_views.del_favorite, name='del_favorite'),
+    path('add_favorite/<int:id>', sub_views.add_favorite, name='add_favorite'),
+    path('legales_notices/', sub_views.legales_notices, name='legales_notices'),
+    # USERS APP
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('legales_notices/', search_views.legales_notices, name='legales_notices'),
+    # SOCIAL AUTH MODULE
+    url(r'^auth/', include('social_django.urls', namespace='social')),
 ]
 
 if settings.DEBUG:
